a7fe4e1966d66462fac15496cf50fb860e89c9cc
Fabien Sanchez <fzed51+git@gmail.com>
2026-04-03 21:14:29 +0200

    feat: mettre à jour la version à 1.1.0 et afficher dynamiquement la version dans la page d'accueil

--- app/pages/Home.tsx
+++ app/pages/Home.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import { version } from "../../package.json"
 
 export default function Home() {
   return (
@@ -8,7 +9,7 @@ export default function Home() {
       
       <div className="home-terminal">
         <div className="terminal-line">Status: ONLINE</div>
-        <div className="terminal-line">Version: 1.0.0</div>
+        <div className="terminal-line">Version: {version}</div>
         <div className="terminal-line">Dernière mise à jour: 22/12/2025</div>
         <div className="terminal-line terminal-prompt">_</div>
       </div>
